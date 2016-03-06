5d3f29ff749b4367b7b41540e0b0861a1472d9fb
pathouse <pat.mcgee@plated.com>
2016-03-06 16:11:43 -0500

    commands, controls, timer

--- commands.js
+++ commands.js
@@ -1,32 +1,60 @@
-var Commands = {
+(function (root, factory) {
+  'use strict'
+  if (typeof define === 'function' && define.amd) define([], factory)
+  else if (typeof exports === 'object') module.exports = factory()
+  else root.Commands = factory()
+}(this, function () {
 
-  start: function () {
-  },
-
-  move: function (direction) {
-  },
-
-  read: function (sections) {
-  },
-
-  startTimer: function () {
-  },
-
-  stopTimer: function () {
-  },
-
-  pauseTimer: function () {
+  var Commands = function (Controls) {
+    this.Controls = Controls;
   }
 
-  silenceAlarm: function () {
+  Commands.prototype.addControls = function (Controls) {
+    this.Controls = Controls;
   },
 
-  describe: function (item) {
+  Commands.prototype.start = function () {
+    this.Controls.loadStep();
+    this.Controls.readAll();
   },
 
-  list: function (itemType) {
+  Commands.prototype.move = function (direction) {
+    this.Controls.changeStep(direction);
+    this.Controls.loadStep();
+    this.Controls.readAll();
   },
 
-  show: function (item) {
-  }
+  Commands.prototype.read = function (sections) {
+    this.Controls.read(sections);
+  },
+
+  Commands.prototype.startTimer = function () {
+    this.Controls.startTimer();
+  };
+
+  Commands.prototype.stopTimer = function () {
+    this.Controls.stopTimer();
+  };
+
+  Commands.prototype.pauseTimer = function () {
+    this.Controls.pauseTimer();
+  };
+
+  Commands.prototype.extendTimer = function () {
+    this.Controls.extendTimer();
+  };
+
+  Commands.prototype.silenceAlarm = function () {
+  };
+
+  Commands.prototype.describe = function (item) {
+  };
+
+  Commands.prototype.list = function (itemType) {
+  };
+
+  Commands.prototype.show = function (item) {
+  };
+
+  return Commands;
 }
--- controls.js
+++ controls.js
@@ -1,22 +1,89 @@
-var _ = require("lodash");
+(function (root, factory) {
+  'use strict'
+  if (typeof define === 'function' && define.amd) define([], factory)
+  else if (typeof exports === 'object') module.exports = factory()
+  else root.Controls = factory(require("lodash"))
+}(this, function (_) {
 
-var Controls = {
-  currentStep: 0,
-
-  setRecipe: function(recipe) {
+  var Controls = function (recipe) {
     this.recipe = recipe;
-  },
+    this.currentStepNumber = 0;
+    this.Timer = new Timer();
+  };
 
-  readStep: function () {
-    var step = _.find(this.recipe.steps, { order: this.currentStep});
-    this.readAll()
-  },
+  Controls.prototype.changeStep = function (change) {
+    this.currentStepNumber = this.currentStepNumber + change;
+  };
 
-  readAll: function () {
-    this.read(["equipment", "ingredients", "instructions"])
-  },
+  Controls.prototype.loadStep = function () {
+    this.step = _.find(this.recipe.steps, { order: this.currentStep});
+  };
 
-  read: function (section) {
-    // text to speach on the section name then description
-  }
+  Controls.prototype.readAll = function () {
+    this.read(["equipment", "ingredients", "instructions"]);
+  };
+
+  Controls.prototype.read = function (sections) {
+    _.each(sections, function (section) {
+      var content = this._getSectionContent(section);
+      // text to speach on the section name then description
+    })
+  };
+
+  Controls.prototype.startTimer = function () {
+    this.activeTimer = new Timer({
+      tick    : 1,
+      onstart : function() { console.log('timer started') },
+      onstop  : function() { console.log('timer stop') },
+      onpause : function() { console.log('timer set on pause') }
+     // onend: activateAlarm
+    });
+    var timerDuration = this.step.timer.minutes * 60
+    this.activeTimer.start(timerDuration)
+  };
+
+  Controls.prototype.extendTimer = function () {
+    this.activeTimer = new Timer({
+      tick    : 1,
+      onstart : function() { console.log('timer started') },
+      onstop  : function() { console.log('timer stop') },
+      onpause : function() { console.log('timer set on pause') }
+      // onend: activateAlarm
+    });
+    var timerDuration = this.step.timer.extensionMinutes * 60
+    // Correct way to do this is actually to add the extension to the duration of the active timer. this is a quick hack for limited functionality
+    this.activeTimer.start(timerDuration)
+  };
+
+  Controls.protototype.pauseTimer = function () {
+    if (!this.activeTimer) return
+    this.activeTimer.pause()
+  };
+
+  Controls.prototype.stopTimer = function () {
+    if (!this.activeTimer) return
+    this.activeTimer.stop()
+  };
+
+  Controls.prototype._getSectionContent = function (section) {
+    var content;
+    if (section == "name") {
+      content = this.recipe.name;
+    } else {
+      var sectionList = this.recipe[section];
+      content = this._arrayToSentences(
+        _.map(sectionList, this._getItemContent)
+      );
+    }
+  };
+
+  Controls.prototype._arrayToSentences = function(array) {
+    return _.join(array, ". ");
+  };
+
+  Controls.prototype._getItemContent = function (item) {
+    return this._arrayToSentences([item.name, item.description]);
+  };
+
+  return Controls;
 }
